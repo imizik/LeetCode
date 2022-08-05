34b7b0ee56fd56e5b6e2fd090c2939e98609ade0
Isaac Chung <Chung.isaac3@gmail.com>
2022-08-05 10:40:55 -0700

    Time: 116 ms (17.05%), Space: 43.7 MB (80.43%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.js
@@ -0,0 +1,26 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(root) {
+	let hash = {};
+
+	const dfs = (node) => {
+		if (hash[node.val]) return hash[node.val];
+		let copy = new Node(node.val, []);
+		hash[copy.val] = copy;
+		for (let neighbor of node.neighbors) {
+			copy.neighbors.push(dfs(neighbor))
+}
+return copy
+}
+return root? dfs(root) : null
+}
