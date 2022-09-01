b0632ea27237d67fb5e3d36df606a12d10346373
Isaac Chung <Chung.isaac3@gmail.com>
2022-09-01 15:07:49 -0700

    Time: 97 ms (49.22%), Space: 43.7 MB (80.93%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+    let result = [];
+    // let order = 2;
+    
+    const dfs = (node, order) => {
+        if (!node) return
+        if (!result[order]) result.push([])
+
+        
+        if (order % 2 === 0) {
+            result[order].push(node.val)
+        } else {
+            result[order].unshift(node.val)
+        }
+        dfs(node.left, order + 1);
+        dfs(node.right, order + 1);
+    }
+    
+    dfs(root, 0)
+    
+    return result
+};
