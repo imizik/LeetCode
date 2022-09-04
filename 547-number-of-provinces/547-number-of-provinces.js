d5a0211eac4f8199e85ed99ed9e80b9fcaafa63d
Isaac Chung <Chung.isaac3@gmail.com>
2022-09-03 22:52:56 -0700

    Time: 136 ms (23.23%), Space: 45.1 MB (42.53%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.js
+++ 547-number-of-provinces/547-number-of-provinces.js
@@ -5,35 +5,31 @@
 
 
 function findCircleNum(M) {
-    // visited set
-    const visited = new Set();
-    // friend circles count
-    let circles = 0;
-	
-    // iterate thru matrix
+    let adj = {};
+    let set = new Set();
+    let count = 0;
+    
     for (let i = 0; i < M.length; i++) {
-        // check if this friend has been visited before
-        if (!visited.has(i)) {
-            // start dfs for this friend
-            dfs(i);
-            // this is another friend circle
-            circles++;
-        }
-    }
-	
-    return circles;
-	
-    // helper method to do dfs traversal thru M
-    function dfs(i) {
-        // go thru this friend's friends
-        for (let j = 0; j < M.length; j++) {
-            // check if this is a friend, and not visited before
-            if (M[i][j] === 1 && !visited.has(j)) {
-                // add as visited
-                visited.add(j);
-                // call dfs
-                dfs(j);
+        for (let j = 0; j < M[0].length; j++) {
+            if (M[i][j] === 1) {
+                adj[i] ? adj[i].push(j) : adj[i] = [j]
             }
         }
     }
+    
+    const dfs = (node) => {
+        set.add(node)
+        for (let num of adj[node]) {
+            if (!set.has(num)) dfs(num)
+        }
+    }
+    
+    for (let key in adj) {
+        if (!set.has(Number(key))) {
+            dfs(Number(key))
+            count++
+        }
+    }
+        
+    return count
 }
