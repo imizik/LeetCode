d206493c0dce692f41dbbba41908f7a2714ad562
Isaac Chung <Chung.isaac3@gmail.com>
2022-09-07 22:57:42 -0700

    Time: 135 ms (31.41%), Space: 45.5 MB (36.05%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.js
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -3,46 +3,39 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
+    let min = 0;
     let oranges = 0;
     let queue = [];
-    let time = 0;
-    
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === 2) {
-                queue.push([i,j, 0])
-            } else if (grid[i][j] === 1) oranges++
+            if (grid[i][j] === 1) oranges++
+            if (grid[i][j] === 2) queue.push([i,j])
         }
     }
     
-    const directions = [[0,1], [0,-1], [1,0], [-1,0]]
-    while (queue.length > 0 && oranges) {
-        const curr = queue.shift()
-        if (grid[curr[0]][curr[1]] === 1) {
-            grid[curr[0]][curr[1]] = 2
-            oranges--
-            time = curr[2]
-        } 
-        for (let direction of directions) {
-            const newRow = curr[0] + direction[0]
-            const newCol = curr[1] + direction[1]
-            if (inBounds(grid, newRow, newCol ) && grid[newRow][newCol] === 1) {
-                queue.push([newRow, newCol, curr[2]+1])
+    while(queue.length) {
+        let directions = [[1,0], [0,1], [-1,0], [0,-1]]
+        
+        let size = queue.length;
+        for (let i = 0; i < size; i++) {
+            let [row, col] = queue.shift()
+            for (let [r,c] of directions) {
+                let newR = row + r
+                let newC = col + c
+                if (inBounds(newR, newC, grid) && grid[newR][newC] === 1) {
+                    grid[newR][newC] = 2
+                    queue.push([newR, newC]);
+                    oranges--
+                }
             }
         }
-
+        if (queue.length > 0) min++
     }
     
-    return oranges === 0 ? time : -1
+    return oranges === 0 ? min : -1
     
 };
-    
-const inBounds = function(grid, row, col) {
-    if (row >=0 && col >=0 && row < grid.length && col < grid[0].length) {
-        return true
-    } else {
-        return false
-    }
+
+function inBounds(row, col, grid) {
+    return row >= 0 && col >= 0 && row < grid.length && col < grid[0].length
 }
-
-
