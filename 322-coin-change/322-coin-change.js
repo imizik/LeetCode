6ee8cafe85520d15c88aeb79cd3ca6f09d2eea17
Isaac Chung <Chung.isaac3@gmail.com>
2022-10-04 20:00:24 -0700

    Time: 445 ms (12.98%), Space: 49.6 MB (28.41%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -3,21 +3,26 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount, memo={}) {
+var coinChange = function(coins, amount, memo = {}) {
     if (amount in memo) return memo[amount]
-    if (amount === 0) return 0;
-    if (amount < 0) return -1;
-    let min = Number.MAX_SAFE_INTEGER
-    let count;
+    if (amount < 0) return -1
+    if (amount === 0) return 0
     
+    let count = null;
     for (let coin of coins) {
-        const newAmount = amount - coin;
-        count = coinChange(coins, newAmount, memo)
-        if (count !== -1) min = Math.min(min, count + 1)
+        let remainder = amount - coin;
+        let counter = coinChange(coins, remainder, memo)
+        
+        if (counter >= 0) {
+            let curr = counter + 1
+            if (count === null || curr < count) {
+                count = curr
+            }
+        }
+        
     }
     
-    (min=== Number.MAX_SAFE_INTEGER) ? memo[amount]=-1 : memo[amount]=min;
-    return memo[amount]
-    
-    
+    let setter = count === null ? -1 : count
+    memo[amount] = setter
+    return setter
 };
